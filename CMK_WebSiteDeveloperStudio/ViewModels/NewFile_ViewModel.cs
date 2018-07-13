337c744bd16aa3791750111e528c58b8701984bf
cmk1988 <cmk88@web.de>
2018-07-14 01:08:16 +0200

    newfile added

--- CMK_WebSiteDeveloperStudio/ViewModels/NewFile_ViewModel.cs
+++ CMK_WebSiteDeveloperStudio/ViewModels/NewFile_ViewModel.cs
@@ -13,6 +13,51 @@ namespace CMK_WebSiteDeveloperStudio.ViewModels
     {
         Core core;
 
+        float columnWidth;
+        public float ColumWidth
+        {
+            get
+            {
+                return columnWidth;
+            }
+            set
+            {
+                columnWidth = value - 30f;
+                ColumWidthThird = (columnWidth) / 3f;
+            }
+        }
+        float columnWidthThird;
+        public float ColumWidthThird
+        {
+            get
+            {
+                return columnWidthThird;
+            }
+            set
+            {
+                columnWidthThird = value;
+                OnPropertyChanged(nameof(ColumWidthThird));
+                OnPropertyChanged(nameof(ColumWidthHalf));
+                OnPropertyChanged(nameof(ColumWidthSixth));
+            }
+        }
+
+        public float ColumWidthHalf
+        {
+            get
+            {
+                return ColumWidth / 2f;
+            }
+        }
+
+        public float ColumWidthSixth
+        {
+            get
+            {
+                return ColumWidth / 4f;
+            }
+        }
+
         public string Filename { get; set; }
 
         public NewFile_ViewModel(Core core)
---
+++ CMK_WebSiteDeveloperStudio/Workers/ProjectFileWorker.cs
@@ -0,0 +1,64 @@
+﻿using CMK_WebSiteDeveloperStudio.DTOs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMK_WebSiteDeveloperStudio.Workers
+{
+    public class ProjectFileWorker
+    {
+        public ProjectFile FileInfos { get; }
+        public bool UnsaveChanges { get; private set; }
+
+        private string content;
+        public string Content
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(content))
+                    content = GetFileAsString();
+                return content;
+            }
+            set
+            {
+                content = value;
+                UnsaveChanges = true;
+            }
+        }
+
+        public ProjectFileWorker(ProjectFile FileInfos)
+        {
+            this.FileInfos = FileInfos;
+        }
+
+        public string GetFileAsString()
+        {
+            using (var fileStream = File.Open(FileInfos.FilePath, FileMode.Open))
+            {
+                using (var reader = new StreamReader(fileStream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        public void Save()
+        {
+            using (var fileStream = File.Open(FileInfos.FilePath, FileMode.Create))
+            {
+                using (var writer = new StreamWriter(fileStream))
+                {
+                    writer.Write(Content);
+                }
+            }
+        }
+
+        public void Create()
+        {
+            Save();
+        }
+    }
+}
