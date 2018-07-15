d212ce7135a512b863ae3cd8dbed76a94dae96ca
cmk1988 <cmk88@web.de>
2018-07-16 00:40:25 +0200

    templatefunnel extended

--- CMK_WebSiteDeveloperStudio/Services/TemplateFunnel.cs
+++ CMK_WebSiteDeveloperStudio/Services/TemplateFunnel.cs
@@ -2,6 +2,7 @@
 using CMK_WebSiteDeveloperStudio.DTOs;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,19 @@ namespace CMK_WebSiteDeveloperStudio.Services
         public List<Template> GetCodeTemplates()
         {
             return templates.Templates.FindAll(x => x.FileTemplate == "2");
+        }
+
+        public string Fill(Template template)
+        {
+            using (var reader = File.OpenText(template.FilePath))
+            {
+                var content = reader.ReadToEnd();
+                foreach (var placeholder in template.PlaceHolders)
+                {
+                    content.Replace(placeholder.Symbol, placeholder.Content);
+                }
+                return content;
+            }
         }
     }
 }
