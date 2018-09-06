81c6005704f91a9169068fda68a1bdc7f100e4b4
cmk1988 <cmk88@web.de>
2018-09-06 20:40:05 +0200

    some changes

---
+++ CMK_WebSiteDeveloperStudio/Services/CodeSchemeProvider.cs
@@ -0,0 +1,14 @@
+﻿using CMK_WebSiteDeveloperStudio.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMK_WebSiteDeveloperStudio.Services
+{
+    public class CodeSchemeProvider
+    {
+        private Dictionary<int, List<ColorScheme>> layers;
+    }
+}
