b9ab1e38e862ae87ee78d35b7f8648a3f181f32d
cmk1988 <cmk88@web.de>
2019-03-05 23:22:22 +0100

    default value added

--- CMK_WebSiteDeveloperStudio/DTOs/ProjectFile.cs
+++ CMK_WebSiteDeveloperStudio/DTOs/ProjectFile.cs
@@ -12,7 +12,7 @@ namespace CMK_WebSiteDeveloperStudio.DTOs
         public string FileExtension => Path.GetExtension(FilePath);
         public string FileName => Path.GetFileName(FilePath);
         public string FilePath { get; set; }
-        public string Tags { get; set; }
+        public string Tags { get; set; } = "";
         public string[] TagCollection => Tags.Split(',');
         public int FileType { get; set; }
         public long LastModified { get; set; }
